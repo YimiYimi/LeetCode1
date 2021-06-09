15411db19b5e6797acbce14caacdeca403140ab7
YimiYimi <942255780@qq.com>
2021-06-09 13:28:28 +0800

    20210609

--- leetcode/editor/cn/[剑指 Offer 26]树的子结构.cpp
+++ leetcode/editor/cn/[剑指 Offer 26]树的子结构.cpp
@@ -46,9 +46,16 @@
  * };
  */
 class Solution {
+private:
+    bool dfs(TreeNode* A, TreeNode* B){
+        if(!B)  return true;
+        if(!A)  return false;
+        return A->val == B->val && dfs(A->left, B->left) && dfs(A->right, B->right);
+    }
 public:
     bool isSubStructure(TreeNode* A, TreeNode* B) {
-
+        if(!A || !B)    return false;
+        return dfs(A, B) ||  isSubStructure(A->left, B) || isSubStructure(A->right, B);
     }
 };
 //leetcode submit region end(Prohibit modification and deletion)
---
+++ leetcode/editor/cn/[剑指 Offer 28]对称的二叉树.cpp
@@ -0,0 +1,66 @@
+//请实现一个函数，用来判断一棵二叉树是不是对称的。如果一棵二叉树和它的镜像一样，那么它是对称的。 
+//
+// 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。 
+//
+// 1 
+// / \ 
+// 2 2 
+// / \ / \ 
+//3 4 4 3 
+//但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的: 
+//
+// 1 
+// / \ 
+// 2 2 
+// \ \ 
+// 3 3 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：root = [1,2,2,3,4,4,3]
+//输出：true
+// 
+//
+// 示例 2： 
+//
+// 输入：root = [1,2,2,null,3,null,3]
+//输出：false 
+//
+// 
+//
+// 限制： 
+//
+// 0 <= 节点个数 <= 1000 
+//
+// 注意：本题与主站 101 题相同：https://leetcode-cn.com/problems/symmetric-tree/ 
+// Related Topics 树 
+// 👍 180 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+//递归法
+class Solution {
+private:
+    bool dfs(TreeNode* A, TreeNode* B){
+        if(!A && !B)    return true;
+        if(!A || !B)    return false;
+        return A->val == B->val && dfs(A->left, B->right) && dfs(A->right, B->left);
+    }
+public:
+    bool isSymmetric(TreeNode* root) {
+        if(!root)   return true;
+        return dfs(root->left, root->right);
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
