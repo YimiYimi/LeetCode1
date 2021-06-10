d8ba42add796c8b4fcfd7d4d9b5866268c11a993
YimiYimi <942255780@qq.com>
2021-06-10 23:00:20 +0800

    20210610

--- leetcode/editor/cn/[剑指 Offer 31]栈的压入、弹出序列.cpp
+++ leetcode/editor/cn/[剑指 Offer 31]栈的压入、弹出序列.cpp
@@ -37,7 +37,16 @@
 class Solution {
 public:
     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
-
+        stack<int> stk;
+        int i = 0;
+        for(int push:pushed){
+            stk.push(push);
+            while(!stk.empty() && stk.top() == popped[i]){
+                stk.pop();
+                i++;
+            }
+        }
+        return stk.empty();
     }
 };
 //leetcode submit region end(Prohibit modification and deletion)
---
+++ leetcode/editor/cn/[剑指 Offer 32 - II]从上到下打印二叉树 II.cpp
@@ -0,0 +1,54 @@
+//从上到下按层打印二叉树，同一层的节点按从左到右的顺序打印，每一层打印到一行。 
+//
+// 
+//
+// 例如: 
+//给定二叉树: [3,9,20,null,null,15,7], 
+//
+//     3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+// 
+//
+// 返回其层次遍历结果： 
+//
+// [
+//  [3],
+//  [9,20],
+//  [15,7]
+//]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 节点总数 <= 1000 
+// 
+//
+// 注意：本题与主站 102 题相同：https://leetcode-cn.com/problems/binary-tree-level-order-tra
+//versal/ 
+// Related Topics 树 广度优先搜索 
+// 👍 115 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
