8421dcb7425c2ee94fc3d8221fbd775b9da5de5b
YimiYimi <942255780@qq.com>
2021-06-21 21:31:00 +0800

    20210621

--- leetcode/editor/cn/[剑指 Offer 42]连续子数组的最大和.cpp
+++ leetcode/editor/cn/[剑指 Offer 42]连续子数组的最大和.cpp
@@ -27,15 +27,18 @@
 
 
 //leetcode submit region begin(Prohibit modification and deletion)
-int maxSubArray(vector<int>& nums) {
-    if(nums.empty())    return 0;
-    int sum = 0, max;
-    max = nums[0];
-    for(int i:nums){
-        sum += i;
-        if(sum < i)  sum = i;
-        if(sum > max)   max = sum;
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        if(nums.empty())    return 0;
+        int sum = 0, max;
+        max = nums[0];
+        for(int i:nums){
+            sum += i;
+            if(sum < i)  sum = i;
+            if(sum > max)   max = sum;
+        }
+        return max;
     }
-    return max;
-}
+};
 //leetcode submit region end(Prohibit modification and deletion)
