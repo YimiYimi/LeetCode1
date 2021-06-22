fab8614ea70827fc7e1cc0166b39e19119f79406
YimiYimi <942255780@qq.com>
2021-06-22 09:55:37 +0800

    20210622

--- leetcode/editor/cn/test.cpp
+++ leetcode/editor/cn/test.cpp
@@ -20,105 +20,24 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
-class Codec {
-public:
-
-    // Encodes a tree to a single string.
-    string serialize(TreeNode* root) {
-        if(!root)   return NULL;
-        queue<TreeNode*> que;
-        string s = "[";
-        que.push(root);
-        while(!que.empty()){
-            s += ",";
-            TreeNode* node = que.front();
-            que.pop();
-            if(!node){
-                s += node->val;
-                que.push(node->left);
-                que.push(node->right);
-            }else{
-                s += "null";
-            }
-        }
-        s += "]";
-        return s;
-    }
-
-    // Decodes your encoded data to tree.
-    TreeNode* deserialize(string data) {
-        vector<string> vec;
-        string tmp;
-        istringstream iss;
-        while(getline(iss, tmp, ','))
-            vec.push_back(tmp);
-        TreeNode* node;
-        return node;
-    }
-};
 class Solution {
 public:
-    vector<string> permutation(string s) {
-        dfs(s, 0);
-        return res;
-    }
-private:
-    vector<string> res;
-    void dfs(string s, int pos){
-        if(pos == s.size()-1){
-            res.push_back(s);
-            return;
+    int findNthDigit(int n) {
+        string s;
+        stringstream ss;
+        for(int i = 0; i <= n; i++){
+            string si;
+            ss << i;
+            ss >> si;
+            s += si;
+            ss.clear(); //不可少
+            if(s.size() > n)   break;
         }
-        set<int> se;
-        for(int i = pos; i < s.size(); i++){
-            //之前已交换过该字母，无需重复交换，剪枝
-            if(se.find(s[i]) != se.end())   continue;
-            se.insert(s[i]);
-            swap(s[pos], s[i]);
-            dfs(s, pos+1);
-            swap(s[pos], s[i]);
-        }
-    }
-};
-class MedianFinder {
-private:
-    priority_queue<int, vector<int>, less<int> > q1;
-    priority_queue<int, vector<int>, greater<int> > q2;
-public:
-    /** initialize your data structure here. */
-    MedianFinder() {
-
-    }
-
-    void addNum(int num) {
-        if(q1.size() == q2.size()){
-            q2.push(num);
-            q1.push(q2.top());
-            q2.pop();
-        }else{
-            q1.push(num);
-            q2.push(q1.top());
-            q1.pop();
-        }
-    }
-
-    double findMedian() {
-        return q1.size()==q2.size()? ((double)q1.top()+q2.top())/2 : q1.top();
+        return s[n]-'0';
     }
 };
 int main(){
-    MedianFinder mf;
-    mf.addNum(2);
-    mf.addNum(3);
-    cout<<mf.findMedian();
+    Solution so;
+    cout << so.findNthDigit(1000000000);
 }
 
